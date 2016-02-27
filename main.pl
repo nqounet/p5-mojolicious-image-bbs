05364268c9c1331e4e61ef0c6e302a1ec28d1d43
nqounet <mail@nqou.net>
2016-02-27 16:11:28 +0900

    update main.pl(master)

--- main.pl
+++ main.pl
@@ -3,4 +3,19 @@ use Mojolicious::Lite;
 
 plugin Mount => +{'/uploader/' => 'script/my_app'};
 
+get '/' => 'index';
+
 app->start;
+__DATA__
+@@ index.html.ep
+<!DOCTYPE html>
+<html>
+<head><title>大きなシステム</title></head>
+<body>
+
+...
+<h1><a href="/uploader/">画像掲示板</a></h1>
+...
+
+</body>
+</html>
